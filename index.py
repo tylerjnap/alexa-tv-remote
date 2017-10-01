d6257285eb498a79bf71c2419c51851d9956d529
Tyler Nappy <tylernappy@gmail.com>
2017-10-01 16:02:32 -0400

    made delay time into a variable

--- index.py
+++ index.py
@@ -10,6 +10,8 @@ from flask_ask import Ask, statement, question, session
 app = Flask(__name__)
 ask = Ask(app, '/alexa-tv-remote')
 
+time_between_press = 0.2
+
 @ask.launch
 def start_skill():
     welcome_message = 'How should the t.v. update?'
@@ -32,39 +34,39 @@ def change_source(source_value):
     # Blink LED
     # if source_value == 'cable box':
     #     os.system('irsend SEND_ONCE ~/lgremote2 KEY_MENU)
-    #     time.sleep(0.5)
+    #     time.sleep(time_between_press)
     #     os.system('irsend SEND_ONCE ~/lgremote2 KEY_DOWN)
-    #     time.sleep(0.5)
+    #     time.sleep(time_between_press)
     #     os.system('irsend SEND_ONCE ~/lgremote2 KEY_DOWN)
-    #     time.sleep(0.5)
+    #     time.sleep(time_between_press)
     #     os.system('irsend SEND_ONCE ~/lgremote2 KEY_OK)
-    #     time.sleep(0.5)
+    #     time.sleep(time_between_press)
     #     os.system('irsend SEND_ONCE ~/lgremote2 KEY_EXIT)
     # elif source_value == 'h.d.m.i. two':
     #     os.system('irsend SEND_ONCE ~/lgremote2 KEY_MENU)
-    #     time.sleep(0.5)
+    #     time.sleep(time_between_press)
     #     os.system('irsend SEND_ONCE ~/lgremote2 KEY_DOWN)
-    #     time.sleep(0.5)
+    #     time.sleep(time_between_press)
     #     os.system('irsend SEND_ONCE ~/lgremote2 KEY_DOWN)
-    #     time.sleep(0.5)
+    #     time.sleep(time_between_press)
     #     os.system('irsend SEND_ONCE ~/lgremote2 KEY_DOWN)
-    #     time.sleep(0.5)
+    #     time.sleep(time_between_press)
     #     os.system('irsend SEND_ONCE ~/lgremote2 KEY_OK)
-    #     time.sleep(0.5)
+    #     time.sleep(time_between_press)
     #     os.system('irsend SEND_ONCE ~/lgremote2 KEY_EXIT)
     # elif source_value == 'x. box':
     #     os.system('irsend SEND_ONCE ~/lgremote2 KEY_MENU)
-    #     time.sleep(0.5)
+    #     time.sleep(time_between_press)
     #     os.system('irsend SEND_ONCE ~/lgremote2 KEY_DOWN)
-    #     time.sleep(0.5)
+    #     time.sleep(time_between_press)
     #     os.system('irsend SEND_ONCE ~/lgremote2 KEY_DOWN)
-    #     time.sleep(0.5)
+    #     time.sleep(time_between_press)
     #     os.system('irsend SEND_ONCE ~/lgremote2 KEY_DOWN)
-    #     time.sleep(0.5)
+    #     time.sleep(time_between_press)
     #     os.system('irsend SEND_ONCE ~/lgremote2 KEY_DOWN)
-    #     time.sleep(0.5)
+    #     time.sleep(time_between_press)
     #     os.system('irsend SEND_ONCE ~/lgremote2 KEY_OK)
-    #     time.sleep(0.5)
+    #     time.sleep(time_between_press)
     #     os.system('irsend SEND_ONCE ~/lgremote2 KEY_EXIT)
 
     text = 'Changing source to {}'.format(source_value)
@@ -92,7 +94,7 @@ def change_volume(volume_value, increase_or_decrease_volume):
 
     # Blink LED
     # for x in range (0, volume_value):
-    #     time.sleep(0.5)
+    #     time.sleep(time_between_press)
     #     os.system('irsend SEND_ONCE ~/lgremote2 {}'.format(key))
 
     text = 'Updating the volume by {}'.format(volume_value)
