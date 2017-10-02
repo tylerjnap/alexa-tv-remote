66e7bc0c7c4ae7faf93bc6f5201317975b0e6f34
Tyler Nappy <tylernappy@gmail.com>
2017-10-01 20:07:33 -0400

    added another variable for time delay between presses

--- blink_ir_led.py
+++ blink_ir_led.py
@@ -1,7 +1,8 @@
 import os
 import time
 
-time_between_press = 0.6
+time_between_press_source = 2.0
+time_between_press_volume = 0.5
 
 # LIRC conf file when finding out blink values for IR LED
 lirc_file_conf = '~/lgremote2'
@@ -12,39 +13,39 @@ def change_power():
 def change_source(source_value):
     if source_value == 'cable box':
         os.system('irsend SEND_ONCE {} KEY_MENU'.format(lirc_file_conf))
-        time.sleep(time_between_press)
+        time.sleep(time_between_press_source)
         os.system('irsend SEND_ONCE {} KEY_DOWN'.format(lirc_file_conf))
-        time.sleep(time_between_press)
+        time.sleep(time_between_press_source)
         os.system('irsend SEND_ONCE {} KEY_DOWN'.format(lirc_file_conf))
-        time.sleep(time_between_press)
+        time.sleep(time_between_press_source)
         os.system('irsend SEND_ONCE {} KEY_OK'.format(lirc_file_conf))
-        time.sleep(time_between_press)
+        time.sleep(time_between_press_source)
         os.system('irsend SEND_ONCE {} KEY_EXIT'.format(lirc_file_conf))
     elif source_value == 'h.d.m.i. two':
         os.system('irsend SEND_ONCE {} KEY_MENU'.format(lirc_file_conf))
-        time.sleep(time_between_press)
+        time.sleep(time_between_press_source)
         os.system('irsend SEND_ONCE {} KEY_DOWN'.format(lirc_file_conf))
-        time.sleep(time_between_press)
+        time.sleep(time_between_press_source)
         os.system('irsend SEND_ONCE {} KEY_DOWN'.format(lirc_file_conf))
-        time.sleep(time_between_press)
+        time.sleep(time_between_press_source)
         os.system('irsend SEND_ONCE {} KEY_DOWN'.format(lirc_file_conf))
-        time.sleep(time_between_press)
+        time.sleep(time_between_press_source)
         os.system('irsend SEND_ONCE {} KEY_OK'.format(lirc_file_conf))
-        time.sleep(time_between_press)
+        time.sleep(time_between_press_source)
         os.system('irsend SEND_ONCE {} KEY_EXIT'.format(lirc_file_conf))
     elif source_value == 'x. box':
         os.system('irsend SEND_ONCE {} KEY_MENU'.format(lirc_file_conf))
-        time.sleep(time_between_press)
+        time.sleep(time_between_press_source)
         os.system('irsend SEND_ONCE {} KEY_DOWN'.format(lirc_file_conf))
-        time.sleep(time_between_press)
+        time.sleep(time_between_press_source)
         os.system('irsend SEND_ONCE {} KEY_DOWN'.format(lirc_file_conf))
-        time.sleep(time_between_press)
+        time.sleep(time_between_press_source)
         os.system('irsend SEND_ONCE {} KEY_DOWN'.format(lirc_file_conf))
-        time.sleep(time_between_press)
+        time.sleep(time_between_press_source)
         os.system('irsend SEND_ONCE {} KEY_DOWN'.format(lirc_file_conf))
-        time.sleep(time_between_press)
+        time.sleep(time_between_press_source)
         os.system('irsend SEND_ONCE {} KEY_OK'.format(lirc_file_conf))
-        time.sleep(time_between_press)
+        time.sleep(time_between_press_source)
         os.system('irsend SEND_ONCE {} KEY_EXIT'.format(lirc_file_conf))
 
 def change_mute():
@@ -57,5 +58,5 @@ def change_volume(volume_value, increase_or_decrease_volume):
         key = 'KEY_VOLUMEDOWN'
 
     for x in range (0, int(volume_value)):
-        time.sleep(time_between_press)
+        time.sleep(time_between_press_volume)
         os.system('irsend SEND_ONCE {} {}'.format(lirc_file_conf, key))
